c0a824db9f1d2ce66b7438aee60a550d52683734
Kimonae <shinokasu@gmail.com>
2022-09-15 09:59:53 +0200

    nav with angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfilComponent } from './profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BienvenueComponent },
+  { path: 'profil', component: ProfilComponent },
+  { path: '**', component: PageNotFoundComponent },
+];//indiquer le chemin d'un composant, si vide alors Bienvenue Comp
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilComponent } from './profil/profil.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilComponent,
+    BienvenueComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
